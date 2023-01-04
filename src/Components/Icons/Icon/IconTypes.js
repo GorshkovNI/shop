74896b82b515217c4144ba03d4f13653e0a33237
Nikita Gorshkov <nigorshkov99@gmail.com>
2023-01-04 17:15:00 +0500

    Card

---
+++ src/Components/Icons/Icon/IconTypes.js
@@ -0,0 +1,7 @@
+import { ReactComponent as Heart } from './heart.svg'
+import { ReactComponent as btnPlus } from './btnPlus.svg'
+
+export const iconsTypes = {
+    heart: Heart,
+    btnPlus: btnPlus
+}
